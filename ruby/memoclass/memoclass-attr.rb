7bbeb6c2869fcfb5c2fe3f52ad6cebb9bbb513f2
Thibaud Colas <thibaudcolas@gmail.com>
2013-01-10 15:24:55 +0100

    Adds memoclass implementations

---
+++ ruby/memoclass/memoclass-attr.rb
@@ -0,0 +1,39 @@
+# Mémo-classe en Ruby : stockage des instances dans la méta-classe.
+#---------------------------------------------------------------------
+# Syntaxe de Ruby (pour le @@) : http://overapi.com/static/cs/RubyCheat.pdf
+
+# La classe qui mémorise ses instances. Sa méta-classe est la classe des classes qui mémorisent leurs instances.
+class Memo
+  # Stockage des instances. @@ = variable de classe.
+  @@instances = []
+
+  # initialize est appelée à chaque new. Le self (instance de Memo) est stocké dans la variable instances de la méta-classe de Memo.
+  def initialize
+    @@instances << self
+  end
+
+  # Retourne @@instances.
+  def self.all_instances
+    @@instances
+  end
+
+  def self.last_instance
+    @@instances.last
+  end
+
+  def talk
+    puts "They remember me."
+  end
+end
+
+# Tests.
+
+a = Memo.new
+p a
+b = Memo.new
+p b
+
+b.talk
+
+p Memo.last_instance
+p Memo.all_instances
---
+++ ruby/memoclass/memoclass-module.rb
@@ -0,0 +1,74 @@
+# Mémo-classe en Ruby : avec un module façon singleton
+#---------------------------------------------------------------------
+# Exemple de proposition de module Multiton : http://www.ruby-forum.com/topic/101486
+
+# Ce module devra être inclus par toutes les classes qui veulent se rappeler de leurs instances.
+module Memo
+
+  # Collecteur d'instances classées par classe.
+  INSTANCES = {}
+
+  # Lien avec le constructeur de chaque classe.
+  MEMO_NEW_HOOK = :memo_new
+
+  # On va étendre les classes qui incluent le module Memo.
+  def self.append_features klass
+
+    # Les instances de la classe actuelle sont un tableau vide.
+    INSTANCES[klass] = []
+
+    # Self devient la classe actuellement étendue.
+    class << klass
+
+      # MEMO_NEW_HOOK devient un alias pour new.
+      alias_method MEMO_NEW_HOOK, :new
+
+      # instance va être appelé à la place de new avec les bons arguments.
+      def instance *args, &block
+        obj = send MEMO_NEW_HOOK, *args, &block
+        # Stockage de l'instance dans INSTANCES[classe courante]
+        INSTANCES[self] << obj
+
+        obj
+      end
+
+      # Retourne INSTANCES[classe courante].
+      def all_instances
+        INSTANCES[self]
+      end
+
+      # On alias #new à notre #instance.
+      alias_method :new, :instance
+    end
+  end
+end
+
+# Tests.
+
+class MemoClass
+  include Memo
+
+  def talk
+    puts "They remember me."
+  end
+end
+
+a = MemoClass.new
+b = MemoClass.new
+c = MemoClass.new
+p a
+p b
+p c
+
+b.talk
+
+p MemoClass.all_instances
+
+class MemoBis
+  include Memo
+end
+
+aa = MemoBis.new
+bb = MemoBis.new
+
+p MemoBis.all_instances
